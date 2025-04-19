b9a72c3cff6219fad919acf20917dfc64137bb30
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-18 21:48:34 -0300

    Cria interface UserRepositoryInterface com métodos abstratos para registro de usuário, recuperação e adição de pedidos

---
+++ src/models/interfaces/user_repository_interface.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from typing import Optional, Tuple
+
+
+class UserRepositoryInterface(ABC):
+    @abstractmethod
+    def registry_user(self, username: str, password: str) -> None:
+        pass
+
+    @abstractmethod
+    def get_user_by_username(self, username: str) -> Tuple[int, str, str]:
+        pass
+
+    @abstractmethod
+    def add_order_to_user(self, user_id: str, order: str) -> None:
+        pass
