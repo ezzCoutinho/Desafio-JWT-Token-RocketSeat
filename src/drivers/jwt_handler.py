001aa8139800863c56fd8c1f18f7b35a937fa0a3
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-19 21:13:47 -0300

    Adiciona a classe JwtHandler para criação e decodificação de tokens JWT, utilizando configurações definidas em jwt_configs.py.

---
+++ src/drivers/jwt_handler.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta, timezone
+from typing import Dict
+
+import jwt
+
+from src.configs.jwt_configs import jwt_infos
+
+
+class JwtHandler:
+    def create_jwt_token(self, body: Dict = {}) -> str:
+        token = jwt.encode(
+            payload={
+                "exp": datetime.now(timezone.utc)
+                + timedelta(hours=float(jwt_infos["JWT_HOURS"])),  # type: ignore
+                **body,
+            },
+            key=jwt_infos["KEY"],
+            algorithm=jwt_infos["ALGORITHM"],
+        )
+        return token
+
+    def decode_jwt_token(self, token: str) -> Dict:
+        return jwt.decode(
+            token,
+            key=jwt_infos["KEY"],
+            algorithms=[jwt_infos["ALGORITHM"]],  # type: ignore
+        )
