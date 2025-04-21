b03b43abcf64ddeb5011ec4c0ebc0c95babeb967
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-21 10:28:31 -0300

    Atualiza o método add_order_to_user na classe OrderHistory para retornar o histórico de pedidos do usuário em formato de lista, melhorando a estrutura da resposta e a manipulação de dados.

--- src/controllers/order_history.py
+++ src/controllers/order_history.py
@@ -1,9 +1,9 @@
+import json
 from typing import Dict
 
 from src.controllers.interfaces.order_history_controller_interface import (
     OrderHistoryControllerInterface,
 )
-
 from src.models.interfaces.user_repository_interface import UserRepositoryInterface
 
 
@@ -13,5 +13,16 @@ class OrderHistory(OrderHistoryControllerInterface):
 
     def add_order_to_user(self, user_id: str, order: str) -> Dict:
         self.__user_repository.add_order_to_user(user_id, order)
+        user = self.__user_repository.get_user_by_username_or_id(user_id=user_id)
+        order_history = user[3]
 
-        return {"type": "User", "count": 1, "order_history": order}
+        try:
+            order_history_list = json.loads(order_history)
+        except:
+            order_history_list = []
+
+        return {
+            "type": "User",
+            "count": len(order_history_list),
+            "order_history": order_history_list,
+        }
